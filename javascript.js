b85c20f93ea40f08e7aa31e0a4304955a240e05a
Andreia Cifoni <andreiacifoni@gmail.com>
2020-11-10 22:11:55 -0500

    learning if, else if, pratical password activity

--- javascript.js
+++ javascript.js
@@ -1,18 +1,42 @@
-// let random = Math.random();
-// if (random < 0.5) {
-//     console.log("your number is less than 0.5")
-//     console.log(random);
-// }
+//  let random = Math.random();
+//  if (random < 0.5) {
+//      console.log("your number is less than 0.5")
+//      console.log(random);
+//  } 
 
-// if (random >= 0.5) {
-//     console.log("your number is greater than 0.5")
-//     console.log(random);
-// }
+//  if (random >= 0.5) {
+//      console.log("your number is greater than 0.5")
+//      console.log(random);
+//  } 
+ 
+ 
 
-const dayOfWeek = "Saturday";
-if (dayOfWeek === "Monday") {
-    console.log("UGHHH I HATE MONDAYS!!")
+//  const dayOfWeek = prompt("enter a day").toLowerCase();
+//  if (dayOfWeek === "monday") {
+//      console.log("UGHHH I HATE MONDAYS!!")
+//  }
+//  else if (dayOfWeek === "saturday") {
+//      console.log("YAY I LOVE SATURDAYS!!")
+//  }
+//  else if (dayOfWeek === "friday") {
+//      console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
+//  }
+//  else {
+//      console.log("MEH")
+//  }
+
+
+const password = prompt ("please enter a new password")
+//password must be 6+ characters
+if(password.length >= 6){
+    //password cannot include space
+    if (password.indexOf(" ") === -1 ){
+        console.log("Valid password")
+    }
+    else {
+        console.log("password cannot contain spaces")
+    }
 }
-else if (dayOfWeek === "Saturday") {
-    console.log("YAY I LOVE SATURDAYS!!")
+else {
+    console.log("Password too short! Must be 6+ characters")
 }
