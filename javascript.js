3045f198a5629557d34a10f2989608ef474b626c
Andreia Cifoni <andreiacifoni@gmail.com>
2020-11-10 21:26:09 -0500

    JS if and else if

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
-let random = Math.random();
-if (random < 0.5) {
-    console.log("your number is less than 0.5")
-    console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("your number is less than 0.5")
+//     console.log(random);
+// }
 
-if (random >= 0.5) {
-    console.log("your number is greater than 0.5")
-    console.log(random);
+// if (random >= 0.5) {
+//     console.log("your number is greater than 0.5")
+//     console.log(random);
+// }
+
+const dayOfWeek = "Saturday";
+if (dayOfWeek === "Monday") {
+    console.log("UGHHH I HATE MONDAYS!!")
+}
+else if (dayOfWeek === "Saturday") {
+    console.log("YAY I LOVE SATURDAYS!!")
 }
