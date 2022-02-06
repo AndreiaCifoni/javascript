c270dda93c971d3217999f6636cef165f7d035bf
Andreia Cifoni <andreiacifoni@gmail.com>
2022-02-05 19:48:32 -0500

    example done

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-//   //**** EXERCICIO COM MEIA RESOLUÇÃO FEITA PELA GENTE... 
+//   //**** EXERCICIO COM MEIA RESOLUÇÃO FEITA PELA GENTE...
 //const form = document.querySelector("#searchForm");
 // form.addEventListener("submit", async function (e) {
 //   e.preventDefault();
@@ -20,15 +20,22 @@
 //   }
 // });
 
-
 const form = document.querySelector("#searchForm");
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
-  );
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  makeImages(res.data);
+  form.elements.query.value = "";
+});
 
-  const img = document.createElement("IMG");
-  img.src = res.data[0].show.image.medium;
-    document.body.append(img);
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
